501f55f60a1fbd6a969d98472cf9faf801f553bd
admin <fauzan4297@gmail.com>
2025-01-13 13:16:07 +0700

    menambahkan MahasiswaApp untuk inisialisasi mengatur komponen yang akan dipakai di seluruh aplikasi

--- app/src/main/java/com/example/connectfirebase/MahasiswaApplications.kt
+++ app/src/main/java/com/example/connectfirebase/MahasiswaApplications.kt
@@ -4,10 +4,11 @@ import android.app.Application
 import com.example.connectfirebase.depedencies.AppContainer
 import com.example.connectfirebase.depedencies.MahasiswaContainer
 
-class MahasiswaApp : Application(){
+//   untuk inisialisasi mengatur komponen yang akan dipakai di seluruh aplikasi
+class MahasiswaApplications: Application() {
     lateinit var container: AppContainer
     override fun onCreate() {
         super.onCreate()
-        container=MahasiswaContainer()
+        container= MahasiswaContainer()
     }
 }
