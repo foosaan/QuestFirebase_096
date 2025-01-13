dd8751b98f7c8b4d41ba9e3f7d333e98e135ffca
admin <fauzan4297@gmail.com>
2025-01-13 13:04:04 +0700

    Membuat interface DestinasiNavigasi sebagai kontrak untuk setiap destinasi navigasi.

---
+++ app/src/main/java/com/example/connectfirebase/ui/navigasi/DestinasiNavigasi.kt
@@ -0,0 +1,14 @@
+package com.example.connectfirebase.ui.navigasi
+
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: String
+}
+object DestinasiHome : DestinasiNavigasi {
+    override val route: String = "home"
+    override val titleRes: String = "Home"
+}
+object DestinasiInsert : DestinasiNavigasi {
+    override val route: String = "insert"
+    override val titleRes: String = "Insert"
+}
