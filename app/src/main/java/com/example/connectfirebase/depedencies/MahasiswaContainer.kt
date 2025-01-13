acaf6c119c9e0ab308f27bf1125ce3e0f381be29
admin <fauzan4297@gmail.com>
2025-01-13 13:17:07 +0700

    menambahkan MahasiswaContainer interface AppContainer untuk menyediakan akses ke MahasiswaRepository.

--- app/src/main/java/com/example/connectfirebase/depedencies/MahasiswaContainer.kt
+++ app/src/main/java/com/example/connectfirebase/depedencies/MahasiswaContainer.kt
@@ -1,17 +1,18 @@
 package com.example.connectfirebase.depedencies
 
 import MahasiswaRepository
-import com.example.connectfirebase.repository.NetworkMahasiswaRespository
+import com.example.connectfirebase.repository.NetworkMahasiswaRepository
 import com.google.firebase.firestore.FirebaseFirestore
 
 
+// interface AppContainer untuk menyediakan akses ke MahasiswaRepository.
 interface AppContainer {
     val mahasiswaRepository: MahasiswaRepository
 }
 
 class MahasiswaContainer: AppContainer {
-    private val firestore : FirebaseFirestore = FirebaseFirestore.getInstance() //Setara dengan url di remote db
+    private val firestore : FirebaseFirestore = FirebaseFirestore.getInstance()
     override val mahasiswaRepository: MahasiswaRepository by lazy {
-        NetworkMahasiswaRespository(firestore)
+        NetworkMahasiswaRepository(firestore)
     }
 }
