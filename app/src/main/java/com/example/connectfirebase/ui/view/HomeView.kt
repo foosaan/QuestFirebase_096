c3762082af201645c327d853578e6fcb191f40f7
admin <fauzan4297@gmail.com>
2025-01-13 13:11:44 +0700

    Menambahkan homeview untuk menampilkan data mahasiswa

--- app/src/main/java/com/example/connectfirebase/ui/view/HomeView.kt
+++ app/src/main/java/com/example/connectfirebase/ui/view/HomeView.kt
@@ -1,9 +1,9 @@
 package com.example.connectfirebase.ui.view
 
-import androidx.compose.foundation.Image
+import android.os.Build
+import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -11,13 +11,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -28,45 +26,41 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.connectfirebase.model.Mahasiswa
 import com.example.connectfirebase.ui.viewmodel.HomeUiState
 import com.example.connectfirebase.ui.viewmodel.HomeViewModel
 
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     navigateToItemEntry: () -> Unit,
     modifier: Modifier = Modifier,
     onDetailClick: (String) -> Unit = {},
-    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
-) {
-    Scaffold (
-        modifier = modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory),
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            TopAppBar(
-                title = {
-                    Text("Home")
-                }
-            )
+            TopAppBar(title = { Text("Home")})
         },
         floatingActionButton = {
             FloatingActionButton(
                 onClick = navigateToItemEntry,
                 shape = MaterialTheme.shapes.medium,
-                modifier = Modifier.padding(18.dp)
+                modifier = Modifier.
+                padding(18.
+                dp)
             ) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = "Add Mahasiswa")
             }
@@ -75,9 +69,10 @@ fun HomeScreen(
         HomeStatus(
             homeUiState = viewModel.mhsUIState,
             retryAction = { viewModel.getMhs() }, modifier = Modifier.padding(innerPadding),
-            onDetailClick = onDetailClick, onDeleteClick = {
-                viewModel.getMhs()
-            }
+            onDetailClick = onDetailClick,
+            onDeleteClick = {
+                viewModel.deleteMhs(it)
+            },
         )
     }
 }
@@ -90,12 +85,13 @@ fun HomeStatus(
     onDeleteClick: (Mahasiswa) -> Unit = {},
     onDetailClick: (String) -> Unit
 ) {
-    when(homeUiState) {
+    when (
+        homeUiState) {
         is HomeUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
-
         is HomeUiState.Success ->
             MhsLayout(
-                mahasiswa = homeUiState.mahasiswa, modifier = modifier.fillMaxWidth(),
+                mahasiswa = homeUiState.mahasiswa,
+                modifier = modifier.fillMaxWidth(),
                 onDetailClick = {
                     onDetailClick(it.nim)
                 },
@@ -103,21 +99,24 @@ fun HomeStatus(
                     onDeleteClick(it)
                 }
             )
+
         is HomeUiState.Error -> OnError(
             message = homeUiState.message.message?:"Error",
             retryAction,
-            modifier = modifier.fillMaxSize())
+            modifier = modifier.fillMaxSize()
+        )
     }
 }
 
 @Composable
 fun OnLoading(modifier: Modifier = Modifier) {
     Column (
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text("Loading...")
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text("Loading.....")
     }
 }
 
@@ -125,15 +124,16 @@ fun OnLoading(modifier: Modifier = Modifier) {
 fun OnError(
     message: String,
     retryAction: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Column (
-        modifier =  modifier,
+    modifier: Modifier = Modifier) {
+    Column(
+        modifier =
+        modifier,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = message, modifier = Modifier.padding(16.dp))
-        Button(onClick = retryAction) {
+        Button(onClick =
+        retryAction) {
             Text("Retry")
         }
     }
@@ -146,7 +146,7 @@ fun MhsLayout(
     onDetailClick: (Mahasiswa) -> Unit,
     onDeleteClick: (Mahasiswa) -> Unit = {}
 ) {
-    LazyColumn (
+    LazyColumn(
         modifier = modifier,
         contentPadding = PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
@@ -157,8 +157,7 @@ fun MhsLayout(
                 modifier = Modifier
                     .fillMaxWidth()
                     .clickable { onDetailClick(mahasiswa) },
-                onDeleteClick = {
-                    onDeleteClick(mahasiswa)
+                onDeleteClick = { onDeleteClick(mahasiswa)
                 }
             )
         }
@@ -168,7 +167,8 @@ fun MhsLayout(
 @Composable
 fun MhsCard(
     mahasiswa: Mahasiswa,
-    modifier: Modifier = Modifier,
+    modifier:
+    Modifier = Modifier,
     onDeleteClick: (Mahasiswa) -> Unit = {}
 ) {
     Card(
@@ -176,45 +176,38 @@ fun MhsCard(
         shape = MaterialTheme.shapes.medium,
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
     ) {
-        Column (
+        Column(
             modifier = Modifier.padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            Row (
+            Row(
                 modifier = Modifier.fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
                     text = mahasiswa.nama,
                     style = MaterialTheme.typography.titleLarge,
-                    modifier = Modifier.weight(1f)
                 )
-
+                Spacer(Modifier.weight(1f))
                 IconButton(onClick = { onDeleteClick(mahasiswa) }) {
                     Icon(
                         imageVector = Icons.Default.Delete,
                         contentDescription = null,
                     )
                 }
-            }
-
-            Column {
                 Text(
                     text = mahasiswa.nim,
                     style = MaterialTheme.typography.titleMedium
                 )
-
-
-                Text(
-                    text = mahasiswa.kelas,
-                    style = MaterialTheme.typography.titleMedium
-                )
-
-                Text(
-                    text = mahasiswa.alamat,
-                    style = MaterialTheme.typography.titleMedium
-                )
             }
+            Text(
+                text = mahasiswa.kelas,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = mahasiswa.alamat,
+                style = MaterialTheme.typography.titleMedium
+            )
         }
     }
 }
