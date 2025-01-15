bc870dec5c4082fb9281cce8ad8bfd83c2fbd4ff
admin <fauzan4297@gmail.com>
2025-01-15 14:16:09 +0700

    Menambahkan object DetailVIewModel

--- app/src/main/java/com/example/connectfirebase/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/connectfirebase/ui/viewmodel/PenyediaViewModel.kt
@@ -1,8 +1,10 @@
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.connectfirebase.MahasiswaApplications
+import com.example.connectfirebase.ui.viewmodel.DetailVIewModel
 import com.example.connectfirebase.ui.viewmodel.HomeViewModel
 import com.example.connectfirebase.ui.viewmodel.InsertViewModel
 
@@ -16,6 +18,12 @@ object PenyediaViewModel {
         }
         initializer {
             InsertViewModel(
+                mahasiswaApplications().container.mahasiswaRepository
+            )
+        }
+        initializer {
+            DetailVIewModel(
+                createSavedStateHandle(),
                 mahasiswaApplications().container.mahasiswaRepository
             )
         }
